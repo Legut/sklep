733bd50160e9751fbdc152b4e970004158da579e
Legut <wlegutowski@gmail.com>
2019-11-01 17:56:27 +0100

    Servlet wyświetlający podstronę, w razie gdy konto nie było wcześniej aktywane

--- src/main/java/session/LoginServlet.java
+++ src/main/java/session/LoginServlet.java
@@ -20,6 +20,7 @@ public class LoginServlet extends HttpServlet {
 	private String pwd;
 	private String user;
 	private String role;
+	private boolean auth;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(request, response);
@@ -36,17 +37,22 @@ public class LoginServlet extends HttpServlet {
 			//TODO: sprawdzanie czy użytkownik jest aktywowany (Jeśli istnieje klucz w polu z
 			// kluczem przy danym uzytkowniku to nieaktywny, jeśli puste pole to aktywny)
 			this.role = LoginDAO.checkRole(user, pwd);
-			HttpSession session = request.getSession();
-			session.setAttribute("username", user);
-			session.setAttribute("role", role);
-			//setting session to expiry in 30 mins
-			session.setMaxInactiveInterval(30*60);
-			Cookie userName = new Cookie("user", user);
-			Cookie userRole = new Cookie("role", role);
-			userName.setMaxAge(30*60);
-			response.addCookie(userName);
-			response.addCookie(userRole);
-			response.sendRedirect("/login-success");
+			this.auth = LoginDAO.checkAuth(user, pwd);
+			if(auth){
+				HttpSession session = request.getSession();
+				session.setAttribute("username", user);
+				session.setAttribute("role", role);
+				//setting session to expiry in 30 mins
+				session.setMaxInactiveInterval(30*60);
+				Cookie userName = new Cookie("user", user);
+				Cookie userRole = new Cookie("role", role);
+				userName.setMaxAge(30*60);
+				response.addCookie(userName);
+				response.addCookie(userRole);
+				response.sendRedirect("/login-success");
+			} else {
+				response.sendRedirect("/account-not-verified");
+			}
 		} else {
 			response.sendRedirect("/login-failed");
 		}
