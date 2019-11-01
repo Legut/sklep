061e11e26bad197da7b378285735f4b6f0c2223c
BuildTools <unconfigured@null.spigotmc.org>
2019-11-01 23:59:04 +0100

    Przerobiono trochę Servlet tak aby zapisywał więcej danych o użytkowniku. Teraz pobiera on dane z bazy i zapisuje w ArrayLiście, dzięki czemu nie ma 10 odwołań tylko jedno + jedno do sprawdzenia czy ktoś taki wgl istnieje. Dzięki temu zoptymalizowano ilość odwołań do bazy. Dodatkowo dopisano instrukcje warunkową, która sprawdza czy użytkownik jest aktywowany, jeśli nie to wystosowuje odpowiednie działania.

--- src/main/java/session/LoginServlet.java
+++ src/main/java/session/LoginServlet.java
@@ -4,6 +4,7 @@ import dao.LoginDAO;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -19,38 +20,59 @@ public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private String pwd;
 	private String user;
-	private String role;
-	private boolean auth;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// get request parameters for userID and password
 		this.user = request.getParameter("user");
 		this.pwd = request.getParameter("pwd");
-		response.setContentType("text/html;charset=UTF-8");
-
 		boolean valid = LoginDAO.validate(user, pwd);
+
 		if(valid){
-			//TODO: sprawdzanie czy użytkownik jest aktywowany (Jeśli istnieje klucz w polu z
-			// kluczem przy danym uzytkowniku to nieaktywny, jeśli puste pole to aktywny)
-			this.role = LoginDAO.checkRole(user, pwd);
-			this.auth = LoginDAO.checkAuth(user, pwd);
-			if(auth){
-				HttpSession session = request.getSession();
-				session.setAttribute("username", user);
-				session.setAttribute("role", role);
-				//setting session to expiry in 30 mins
+			ArrayList<String> userData = LoginDAO.checkUserData(user, pwd);
+			String user_login = userData.get(0);
+			String user_email = userData.get(1);
+			String user_activation_key = userData.get(2);
+			String user_role = userData.get(3);
+			HttpSession session = request.getSession();
+
+			if(user_activation_key.isEmpty()){
+				session.setAttribute("user_login", user_login);
+				session.setAttribute("user_role", user_role);
+				session.setAttribute("user_email", user_email);
+
+				//expiration after: 30 mins
 				session.setMaxInactiveInterval(30*60);
-				Cookie userName = new Cookie("user", user);
-				Cookie userRole = new Cookie("role", role);
+				Cookie userName = new Cookie("user_login", user_login);
+				Cookie userRole = new Cookie("user_role", user_role);
+				Cookie userEmail = new Cookie("user_email", user_email);
+
 				userName.setMaxAge(30*60);
+				userRole.setMaxAge(30*60);
+				userEmail.setMaxAge(30*60);
+
 				response.addCookie(userName);
 				response.addCookie(userRole);
+				response.addCookie(userEmail);
+
 				response.sendRedirect("/login-success");
 			} else {
+				session.setAttribute("user_activation_key", user_activation_key);
+				session.setAttribute("user_email", user_email);
+
+				//expiration after: 30 mins
+				session.setMaxInactiveInterval(30*60);
+				Cookie userActivationKey = new Cookie("user_activation_key", user_activation_key);
+				Cookie userEmail = new Cookie("user_email", user_email);
+
+				userActivationKey.setMaxAge(30*60);
+				userEmail.setMaxAge(30*60);
+
+				response.addCookie(userActivationKey);
+				response.addCookie(userEmail);
+
 				response.sendRedirect("/account-not-verified");
 			}
 		} else {
