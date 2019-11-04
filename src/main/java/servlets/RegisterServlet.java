cb336ea3d84573a82afb7ea7456a103513c3a6fe
BuildTools <unconfigured@null.spigotmc.org>
2019-11-04 06:56:36 +0100

    Rejestracja, wyświetlanie komunikatów i ogólnie cała procedura

--- src/main/java/servlets/RegisterServlet.java
+++ src/main/java/servlets/RegisterServlet.java
@@ -1,6 +1,7 @@
 package servlets;
 
 import dao.RegisterDAO;
+import org.apache.commons.validator.routines.EmailValidator;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -22,24 +23,39 @@ public class RegisterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         this.user_login = request.getParameter("userlogin");
         this.user_pass = request.getParameter("pwd");
-        this.first_name = request.getParameter("fistname");
+        this.first_name = request.getParameter("firstname");
         this.last_name = request.getParameter("lastname");
         this.user_email = request.getParameter("email");
 
-        response.setContentType("text/html;charset=UTF-8");
-        boolean valid_login = RegisterDAO.validateUserLogin(user_login);
-        boolean valid_email = RegisterDAO.validateUserEmail(user_email);
-        if(valid_login){
-            if(valid_email) {
-                RegisterDAO.addUser(user_login, user_pass, first_name, last_name, user_email);
-                response.sendRedirect("/register-success");
+        EmailValidator validator = EmailValidator.getInstance();
+        if (validator.isValid(user_email)) {
+            Boolean valid_login = RegisterDAO.validateUserLogin(user_login);
+            Boolean valid_email = RegisterDAO.validateUserEmail(user_email);
+            if(valid_login==null || valid_email==null) {
+                request.setAttribute("message", "Wystąpił problem w trakcie rejestracji. Skontaktuj się z administratorem.");
+                doGet(request, response);
             } else {
-                response.sendRedirect("/register-failed");
-                //TODO: info, że nie udało się bo na dany email jest już jedno konto
+                if(valid_login==true){
+                    if(valid_email==true) {
+                        String activation_key = RegisterDAO.addUser(user_login, user_pass, first_name, last_name, user_email);
+                        if(activation_key!=null) {
+                            response.sendRedirect("/register-success");
+                        } else {
+                            request.setAttribute("message", "Coś poszło nie tak w trakcie dodawania użytkownika do bazy. Spróbuj ponownie lub skontaktuj się z administratoracją sklepu.");
+                            doGet(request, response);
+                        }
+                    } else {
+                        request.setAttribute("message", "Istnieje już uzytkownik o podanym emailu");
+                        doGet(request, response);
+                    }
+                } else {
+                    request.setAttribute("message", "Podana nazwa użytkownika jest zajęta");
+                    doGet(request, response);
+                }
             }
         } else {
-            response.sendRedirect("/register-failed");
-            // TODO: info, że nie udało się bo login jest już zajęty
+            request.setAttribute("message", "Podany email jest błędny.");
+            doGet(request, response);
         }
     }
 }
--- src/main/webapp/WEB-INF/pages/register.jsp
+++ src/main/webapp/WEB-INF/pages/register.jsp
@@ -11,13 +11,23 @@
     <div class="container-inner">
         <h1>Rejestracja</h1>
         <form action="${pageContext.request.contextPath}/register" method="post">
-            <p><span>Login:</span> <br /> <input type="text" name="userlogin"></p>
-            <p><span>Hasło:</span> <br /> <input type="password" name="pwd"></p>
+            <p><span>Login:</span> <br /> <span style="font-size: 8px">Login musi zawierać minimum 6 znaków.</span> <br />
+                <input type="text" name="userlogin" pattern=".{6,}" title="Login musi zawierać minimum 6 znaków"></p>
+            <p><span>Hasło </span> <br /> <span style="font-size: 8px">Hasło musi zawierać przynajmniej jedną cyfrę jedną wielką i jedną małą literę.<br>Dodatkowo hasło
+                musi składać się z minimum 8 znaków.</span> <br /> <input type="password" name="pwd" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="
+                Hasło musi zawierać przynajmniej jedną cyfrę jedną wielką i jedną małą literę. Dodatkowo hasło musi składać się z minimum 8 znaków."></p>
             <p><span>Imię:</span> <br /> <input type="text" name="firstname"></p>
             <p><span>Nazwisko:</span> <br /> <input type="text" name="lastname"></p>
-            <p><span>Email:</span> <br /> <input type="text" name="email"></p>
+            <p><span>Email:</span> <br /> <input type="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"></p>
             <p><input type="submit" value="Zarejestruj"></p>
         </form>
+        <p style="color: red; font-weight: bold">
+            <%
+                if(request.getAttribute("message")!=null){
+                    out.println(request.getAttribute("message"));
+                }
+            %>
+        </p>
     </div>
 </div>
 </body>
